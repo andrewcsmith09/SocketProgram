acfd304aa0a9a33fa384802cbe54ff685d02428e
andrewcsmith09 <153587771+andrewcsmith09@users.noreply.github.com>
2023-12-12 01:47:49 -0500

    Add files via upload

---
+++ client.cpp
@@ -0,0 +1,70 @@
+// CODE FOR CLIENT SERVER - ANDREW SMITH
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+const int PORT = 60000;
+const unsigned char SPECIAL_FLAG[] = {0xF0, 0xF0, 0xF0, 0xF0, 0xFE, 0xFE, 0xFE, 0xFE};
+
+void printAndSaveWebpage(const char* webpageContent, size_t contentSize) {
+    // Print the webpage content
+    std::cout << "Received webpage content:\n" << webpageContent << std::endl;
+
+    // Save the webpage content to a local file
+    std::ofstream outputFile("webpage_content.html", std::ios::binary);
+    outputFile.write(webpageContent, contentSize);
+    outputFile.close();
+
+    std::cout << "Webpage content saved to 'webpage_content.html'" << std::endl;
+}
+
+int main() {
+    // Create a socket
+    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket == -1) {
+        perror("Error creating socket");
+        return -1;
+    }
+
+    // Set up server address
+    sockaddr_in serverAddress{};
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(PORT);
+    inet_pton(AF_INET, "127.0.0.1", &serverAddress.sin_addr);
+
+    // Connect to the server
+    if (connect(clientSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1) {
+        perror("Error connecting to server");
+        close(clientSocket);
+        return -1;
+    }
+
+    // Assemble the command string
+    const char* url = "https://www2.brockport.edu/";
+    char commandString[1024];
+    memcpy(commandString, SPECIAL_FLAG, sizeof(SPECIAL_FLAG));
+    size_t urlLength = strlen(url);
+    memcpy(commandString + sizeof(SPECIAL_FLAG), &urlLength, sizeof(urlLength));
+    memcpy(commandString + sizeof(SPECIAL_FLAG) + sizeof(urlLength), url, urlLength);
+
+    // Send the command string to the server
+    send(clientSocket, commandString, sizeof(SPECIAL_FLAG) + sizeof(urlLength) + urlLength, 0);
+
+    // Receive and print/save the server's response (webpage content)
+    char buffer[4096];  // Adjust buffer size based on expected webpage content size
+    ssize_t bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0);
+    if (bytesRead <= 0) {
+        perror("Error receiving data");
+    } else {
+        // Call the function to print and save the webpage content
+        printAndSaveWebpage(buffer, static_cast<size_t>(bytesRead));
+    }
+
+    // Close the socket
+    close(clientSocket);
+
+    return 0;
+}
+
---
+++ server.cpp
@@ -0,0 +1,136 @@
+// CODE FOR SERVER - ANDREW SMITH
+#include <iostream>
+#include <cstring>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <cstdio>
+#include <curl/curl.h>
+#include <thread>
+#include <vector>
+
+const int PORT = 60000;
+const unsigned char SPECIAL_FLAG[] = {0xF0, 0xF0, 0xF0, 0xF0, 0xFE, 0xFE, 0xFE, 0xFE};
+
+size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* output) {
+    size_t totalSize = size * nmemb;
+    output->append(static_cast<char*>(contents), totalSize);
+    return totalSize;
+}
+
+std::string fetchWebpage(const std::string& url) {
+    CURL* curl;
+    CURLcode res;
+
+    std::string webpageContent;
+
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    curl = curl_easy_init();
+
+    if (curl) {
+        // Set the URL
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+
+        // Set the callback function to write the received data to webpageContent
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &webpageContent);
+
+        // Perform the request
+        res = curl_easy_perform(curl);
+
+        // Check for errors
+        if (res != CURLE_OK) {
+            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+        }
+
+        // Clean up
+        curl_easy_cleanup(curl);
+    }
+
+    curl_global_cleanup();
+
+    return webpageContent;
+}
+
+void handleClient(int clientSocket) {
+    // Receive command string from the client
+    char buffer[1024];
+    ssize_t bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0);
+    if (bytesRead <= 0) {
+        perror("Error receiving data");
+        close(clientSocket);
+        return;
+    }
+
+    // Check for the special flag
+    if (memcmp(buffer, SPECIAL_FLAG, sizeof(SPECIAL_FLAG)) == 0) {
+        // Extract URL from the command string
+        size_t urlLength;
+        memcpy(&urlLength, buffer + sizeof(SPECIAL_FLAG), sizeof(urlLength));
+        std::string url(buffer + sizeof(SPECIAL_FLAG) + sizeof(urlLength), urlLength);
+
+        // Fetch the webpage content
+        std::string webpageContent = fetchWebpage(url);
+
+        // Send the webpage content back to the client
+        send(clientSocket, webpageContent.c_str(), webpageContent.size(), 0);
+    } else {
+        std::cerr << "Invalid command string" << std::endl;
+    }
+
+    // Close the client socket
+    close(clientSocket);
+}
+
+int main() {
+    // Create a socket
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket == -1) {
+        perror("Error creating socket");
+        return -1;
+    }
+
+    // Bind the socket to an IP address and port
+    sockaddr_in serverAddress{};
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    serverAddress.sin_port = htons(PORT);
+
+    if (bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1) {
+        perror("Error binding socket");
+        close(serverSocket);
+        return -1;
+    }
+
+    // Listen for incoming connections
+    if (listen(serverSocket, 5) == -1) {
+        perror("Error listening");
+        close(serverSocket);
+        return -1;
+    }
+
+    std::cout << "Server listening on port " << PORT << std::endl;
+
+    // Accept connections and handle communication in separate threads
+    while (true) {
+        sockaddr_in clientAddress{};
+        socklen_t clientAddressLength = sizeof(clientAddress);
+
+        // Accept a connection
+        int clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddress, &clientAddressLength);
+        if (clientSocket == -1) {
+            perror("Error accepting connection");
+            continue;
+        }
+
+        std::cout << "Client connected" << std::endl;
+
+        // Create a new thread for each client
+        std::thread(handleClient, clientSocket).detach();
+    }
+
+    // Close the server socket
+    close(serverSocket);
+
+    return 0;
+}
+
